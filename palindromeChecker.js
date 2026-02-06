f6a406e2a6999f4872b730a13c573cdce78409d1
Yakamozm <joshuafollente@gmail.com>
2026-02-06 14:06:53 -0800

    Added space exclusion
    Modified the JavaScript code to exclude spaces from the user's input.

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -2,16 +2,24 @@ let input = prompt("Check if a word is a palindrome! (Case Sensitive)");
 
 
 if (typeof input === "string") {
+    let newInput = "";
     let palindromeVer = "";
     for (i=0; i<input.length; i++) {
-        palindromeVer = palindromeVer + input[input.length-i-1];
+        if (input[i] === " ") {
+            continue;
+        } else {
+            newInput = newInput + input[i];
+        }
     }
-    if (input === palindromeVer) {
+    for (i=0; i<newInput.length; i++) {
+        palindromeVer = palindromeVer + newInput[newInput.length-i-1];
+    }
+    if (newInput === palindromeVer) {
         console.log("This is a palindrome!");
     } else {
         console.log("This is not a palindrome.");
     }
-    console.log(`Original: ${input} Reverse: ${palindromeVer}`)
+    console.log(`Original: ${newInput} Reverse: ${palindromeVer}`)
 } else {
     console.log("invalid input");
 }
